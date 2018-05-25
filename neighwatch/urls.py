c73cdf2129b480b9647908d4e326f667c9964b64
sami-mai <samirah.maison@gmail>
2018-05-25 11:58:22 +0300

    add project urls

--- neighwatch/urls.py
+++ neighwatch/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
+from . import views as project_views
 
 urlpatterns = [
+    url(r'^$', project_views.landing, name='landing'),
     url(r'^admin/', admin.site.urls),
+    # url(r'^new/'), include('burbnews.urls'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
