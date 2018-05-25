b1d1c5babab3f748676331a49aaf8aa78c09bfc4
sami-mai <samirah.maison@gmail>
2018-05-25 11:57:20 +0300

    add project view functions

---
+++ neighwatch/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def landing(request):
+    title = "Home"
+    context = {"title": title}
+
+    return render(request, 'landing.html', context)
