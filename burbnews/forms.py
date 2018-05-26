ce34ab057c3188f0f9f28bd4c7f242c4879528af
sami-mai <samirah.maison@gmail>
2018-05-26 20:36:33 +0300

    add forms

--- burbnews/forms.py
+++ burbnews/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-# from .models import 
+from burbnews import models
 
 
 class NewsLetterForm(forms.Form):
@@ -7,10 +7,17 @@ class NewsLetterForm(forms.Form):
     email = forms.EmailField(label='Email')
 
 
-# class NewArticleForm(forms.ModelForm):
-#     class Meta:
-#         model = Article
-#         exclude = ['editor', 'pub_date']
-#         widgets = {
-#             'tags': forms.CheckboxSelectMultiple(),
-#         }
+class PostForm(forms.ModelForm):
+    class Meta:
+        fields = ("message", "topic")
+        model = models.Post
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop("user", None)
+        super().__init__(*args, **kwargs)
+        if user is not None:
+            self.fields["topic"].queryset = (
+                models.Topic.objects.filter(
+                    pk__in=user.topics.values_list("topic__pk")
+                )
+            )
