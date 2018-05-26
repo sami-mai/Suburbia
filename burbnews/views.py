0181bbb1d5ccd952484fc0abc87ec32f803170ce
sami-mai <samirah.maison@gmail>
2018-05-26 20:38:29 +0300

    add views

--- burbnews/views.py
+++ burbnews/views.py
@@ -1,41 +1,155 @@
-from django.shortcuts import render, redirect
+from braces.views import SelectRelatedMixin
+from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
-from django.db import transaction
-# from .models import
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.urlresolvers import reverse, reverse_lazy
+from django.db import IntegrityError
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
 # from .forms import
+from .models import Topic, TopicMember
+from . import models
+from . import forms
 
 
-@login_required(login_url='/accounts/login/')
-def home(request, id):
-    title = "Rider"
-    current_user = request.user
-    # profile = Rider_Profile.objects.get(name=current_user.id)
-    context = {"title": title, "current_user": current_user, "profile": profile}
-
-    return render(request, 'index-rider.html', context)
+User = get_user_model()
 
 
 # @login_required(login_url='/accounts/login/')
-# @transaction.atomic
-# def edit_profile(request, id):
+# def home(request, id):
+#     title = "Rider"
 #     current_user = request.user
-#     profile = Rider_Profile.objects.get(name=current_user.id)
-#     if request.method == 'POST':
-#         profile_form = EditProfile(request.POST, request.FILES, instance=current_user.profile)
-#         user_form = EditUserForm(request.POST, instance=current_user)
-#         if profile_form.is_valid() and user_form.is_valid():
-#             profile = profile_form.save(commit=False)
-#             user = user_form.save(commit=False)
-#             profile.user = current_user
-#             profile.save()
-#             user.save()
-#             return redirect('home', current_user.id)
-#     else:
-#         profile_form = EditProfile()
-#         user_form = EditUserForm()
+#     # profile = Rider_Profile.objects.get(name=current_user.id)
+#     context = {"title": title, "current_user": current_user}  #, "profile": profile}
 #
-#     context = {
-#         "current_user": current_user, "profile_form": profile_form,
-#         "user_form": user_form, "profile": profile,
-#         }
-#     return render(request, 'edit-riderprofile.html', context)
+#     return render(request, 'index-rider.html', context)
+
+
+class CreateTopic(LoginRequiredMixin, generic.CreateView):
+    fields = ("name", "description")
+    model = Topic
+
+
+class SingleTopic(generic.DetailView):
+    model = Topic
+
+
+class ListTopics(generic.ListView):
+    model = Topic
+
+
+class JoinTopic(LoginRequiredMixin, generic.RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse("topics:single", kwargs={"slug": self.kwargs.get("slug")})
+
+    def get(self, request, *args, **kwargs):
+        topic = get_object_or_404(Topic, slug=self.kwargs.get("slug"))
+
+        try:
+            TopicMember.objects.create(user=self.request.user, topic=topic)
+
+        except IntegrityError:
+            messages.warning(self.request, ("Warning, already a member of {}".format(topic.name)))
+
+        else:
+            messages.success(self.request, "You are now a member of the {} topic.".format(topic.name))
+
+        return super().get(request, *args, **kwargs)
+
+
+class LeaveTopic(LoginRequiredMixin, generic.RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse("topics:single", kwargs={"slug": self.kwargs.get("slug")})
+
+    def get(self, request, *args, **kwargs):
+
+        try:
+
+            membership = models.TopicMember.objects.filter(
+                user=self.request.user,
+                topic__slug=self.kwargs.get("slug")
+            ).get()
+
+        except models.TopicMember.DoesNotExist:
+            messages.warning(
+                self.request,
+                "You can't leave this topic because you aren't in it."
+            )
+        else:
+            membership.delete()
+            messages.success(
+                self.request,
+                "You have successfully left this topic."
+            )
+        return super().get(request, *args, **kwargs)
+
+
+class PostList(SelectRelatedMixin, generic.ListView):
+    model = models.Post
+    select_related = ("user", "topic")
+
+
+class UserPosts(generic.ListView):
+    model = models.Post
+    template_name = "posts/user_post_list.html"
+
+    def get_queryset(self):
+        try:
+            self.post_user = User.objects.prefetch_related("posts").get(
+                username__iexact=self.kwargs.get("username")
+            )
+        except User.DoesNotExist:
+            raise Http404
+        else:
+            return self.post_user.posts.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["post_user"] = self.post_user
+        return context
+
+
+class PostDetail(SelectRelatedMixin, generic.DetailView):
+    model = models.Post
+    select_related = ("user", "topic")
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(
+            user__username__iexact=self.kwargs.get("username")
+        )
+
+
+class CreatePost(LoginRequiredMixin, SelectRelatedMixin, generic.CreateView):
+    # form_class = forms.PostForm
+    fields = ('message', 'topic')
+    model = models.Post
+
+    # def get_form_kwargs(self):
+    #     kwargs = super().get_form_kwargs()
+    #     kwargs.update({"user": self.request.user})
+    #     return kwargs
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return super().form_valid(form)
+
+
+class DeletePost(LoginRequiredMixin, SelectRelatedMixin, generic.DeleteView):
+    model = models.Post
+    select_related = ("user", "topic")
+    success_url = reverse_lazy("posts:all")
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(user_id=self.request.user.id)
+
+    def delete(self, *args, **kwargs):
+        messages.success(self.request, "Post Deleted")
+        return super().delete(*args, **kwargs)
