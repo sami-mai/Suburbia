9a5abe0f9f8a34ce253b92a7f84babffdaddc208
sami-mai <samirah.maison@gmail>
2018-05-26 20:39:08 +0300

    add urls

--- burbnews/urls.py
+++ burbnews/urls.py
@@ -5,8 +5,19 @@ from django.conf.urls.static import static
 
 urlpatterns = [
 
-    url(r'^news/(\d+)', news_views.home, name='news_home'),
-    # url(r'^edit-profile/(\d+)', news_views.edit_profile, name='edit_userprofile'),
+    # url(r'^(\d+)', news_views.home, name='news_home'),
+    url(r'^topics/$', news_views.ListTopics.as_view(), name="all_topics"),
+    url(r'^topics/new_topic/$', news_views.CreateTopic.as_view(), name="create_topic"),
+    url(r'^topics/posts/in/(?P<slug>[-\w]+)/$', news_views.SingleTopic.as_view(), name="single_topic"),
+    url(r'topics/join/(?P<slug>[-\w]+)/$', news_views.JoinTopic.as_view(), name="join_topic"),
+    url(r'topics/leave/(?P<slug>[-\w]+)/$', news_views.LeaveTopic.as_view(), name="leave_topic"),
+
+    url(r'^posts/$', news_views.PostList.as_view(), name="all_posts"),
+    url(r'posts/new_post/$', news_views.CreatePost.as_view(), name="create_post"),
+    url(r'posts/by/(?P<username>[-\w]+)/$', news_views.UserPosts.as_view(), name="for_user"),
+    url(r'posts/by/(?P<username>[-\w]+)/(?P<pk>\d+)/$', news_views.PostDetail.as_view(), name="single_post"),
+    url(r'posts/delete/(?P<pk>\d+)/$', news_views.DeletePost.as_view(), name="delete_post"),
+
 ]
 
 
