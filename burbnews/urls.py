ae2ce6b6af097ab56e5781120e8f2e1ce70df846
sami-mai <samirah.maison@gmail>
2018-05-27 10:04:45 +0300

    update regex in urls

--- burbnews/urls.py
+++ burbnews/urls.py
@@ -9,14 +9,14 @@ urlpatterns = [
     url(r'^topics/$', news_views.ListTopics.as_view(), name="all_topics"),
     url(r'^topics/new_topic/$', news_views.CreateTopic.as_view(), name="create_topic"),
     url(r'^topics/posts/in/(?P<slug>[-\w]+)/$', news_views.SingleTopic.as_view(), name="single_topic"),
-    url(r'topics/join/(?P<slug>[-\w]+)/$', news_views.JoinTopic.as_view(), name="join_topic"),
-    url(r'topics/leave/(?P<slug>[-\w]+)/$', news_views.LeaveTopic.as_view(), name="leave_topic"),
+    url(r'^topics/join/(?P<slug>[-\w]+)/$', news_views.JoinTopic.as_view(), name="join_topic"),
+    url(r'^topics/leave/(?P<slug>[-\w]+)/$', news_views.LeaveTopic.as_view(), name="leave_topic"),
 
     url(r'^posts/$', news_views.PostList.as_view(), name="all_posts"),
-    url(r'posts/new_post/$', news_views.CreatePost.as_view(), name="create_post"),
-    url(r'posts/by/(?P<username>[-\w]+)/$', news_views.UserPosts.as_view(), name="for_user"),
-    url(r'posts/by/(?P<username>[-\w]+)/(?P<pk>\d+)/$', news_views.PostDetail.as_view(), name="single_post"),
-    url(r'posts/delete/(?P<pk>\d+)/$', news_views.DeletePost.as_view(), name="delete_post"),
+    url(r'^posts/new_post/$', news_views.CreatePost.as_view(), name="create_post"),
+    url(r'^posts/by/(?P<username>[-\w]+)/$', news_views.UserPosts.as_view(), name="for_user"),
+    url(r'^posts/by/(?P<username>[-\w]+)/(?P<pk>\d+)/$', news_views.PostDetail.as_view(), name="single_post"),
+    url(r'^posts/delete/(?P<pk>\d+)/$', news_views.DeletePost.as_view(), name="delete_post"),
 
 ]
 
