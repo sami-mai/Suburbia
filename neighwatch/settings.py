cf72a4a7d4c2513f8faae941e59363d1bc9be332
sami-mai <samirah.maison@gmail>
2018-05-28 17:58:44 +0300

    deploying to heroku

--- neighwatch/settings.py
+++ neighwatch/settings.py
@@ -100,9 +100,10 @@ DATABASES = {
           'PASSWORD': config('DB_PASSWORD'),
           }
 }
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config()
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
